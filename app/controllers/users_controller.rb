78b4a43c499806140e98993fc525f204958eb936
康介 <irohalove2525@gmail.com>
2020-10-11 08:55:36 +0000

    5.2終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,14 @@ class UsersController < ApplicationController
   
   
   def new
+    @user = User.new
   end
   
- 
+ def create
+   @user = User.new(params[:user])
+   if @user.save
+   else
+     render :new
+   end
+ end
 end
