e65029b1f032314b46b54d9e2b03a72f164db5dc
康介 <irohalove2525@gmail.com>
2020-10-23 09:14:40 +0000

    9.2.2 表示に対応しよう　終了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201022072357) do
+ActiveRecord::Schema.define(version: 20201023090057) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20201022072357) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "department"
+    t.datetime "basic_time", default: "2020-10-22 23:00:00"
+    t.datetime "worktime", default: "2020-10-22 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
